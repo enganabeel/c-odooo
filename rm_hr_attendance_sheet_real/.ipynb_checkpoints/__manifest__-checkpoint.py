c59aaadd1130d5a35ad1ecda8eb8022a45b55c12
enganabeel <eng_anabeel@outlook.com>
2022-01-21 21:25:19 +0200

    aaa

---
+++ rm_hr_attendance_sheet_real/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HR Attendance Sheet And Policies",
+
+    'summary': """Managing  Attendance Sheets for Employees
+        """,
+    'description': """
+        Employees Attendance Sheet Management   
+    """,
+    'author': "Ramadan Khalil",
+    'website': "rkhalil1990@gmail.com",
+    'price': 99,
+    'currency': 'EUR',
+
+    'category': 'hr',
+    'version': '14.001',
+    'images': ['static/description/bannar.jpg'],
+
+    'depends': ['base',
+                'hr',
+                'hr_payroll',
+                'hr_holidays',
+                'hr_attendance',
+                'bonus_request',
+                'penalty_request',
+                'business_trip',
+                'salary_calcs_real',
+                'contract_wage_fields'],
+    'data': [
+        'data/ir_sequence.xml',
+        'data/data.xml',
+        'data/attendance_data2.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'wizard/change_att_data_view.xml',
+        'views/hr_attendance_sheet_view.xml',
+        'views/hr_attendance_policy_view.xml',
+        'views/hr_contract_view.xml',
+        'views/hr_public_holiday_view.xml',
+        'views/attendance_sheet_batch_view.xml',
+
+    ],
+
+    'license': 'OPL-1',
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
